dea05830f7b72a4c9707378a0db202218ffa4416
peterkrumb <petekrumb@gmail.com>
2019-12-22 23:05:21 -0600

    finished readme and added other functions

--- README.md
+++ README.md
@@ -4,4 +4,13 @@
 For this activity we will be activating a day planner with the functionality of saving events for the day that the user types in and saving them with the help of local storage. I say activate because the styling has been provided to me already, I only need to 
 
 ## Date
-Firstly I wanted to get the date to display at the top. This being a new topic, I searched google and came across a function that allowed me to do just that. It relied on going into the user's browser data and pulling the current date and time zone, and then displaying it via inner.
+First I wanted to get the date to display at the top. For this I used moment.js, a tool for working with dates in JavaScript using the browser's time zone and date settings.
+
+## Table
+For the rows and columns I knew I needed to use addClass since the styling was given to me in the style.css in class format. So I set variables for the tables and rows equal to <td> and <tr> and continued to append them into the container as well as the meeting text area, hour box and button into the rows.
+
+## Save Button
+The save button was an onclick function that added whatever was typed into the meeting text into the local storage. The renderMeetings function was was kept the meetings pinned to the column
+
+## Column coloring
+The column.each function checked each column's data to see if the current hour, which had to be put in a variable of 9 - 17 was greater than, equal to, or less than the currentHour which was known through moment, and shaded the box accordingly.
--- script.js
+++ script.js
@@ -1,15 +1,13 @@
+// moment formatting
 var jumboDate = moment().format("dddd, MMMM D YYYY");
 var currentHour = (moment().format("HH"));
 $("#currentDay").text(jumboDate);
 
-
-
 var scheduleHr = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
 var localHr = ["0900", "1000", "1100", "1200", "100", "200", "300", "400", "500"];
-
+var compareHr = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 for (i = 0; i< scheduleHr.length; i++) {
-    
     var newTable = $("<table>");
     newTable.addClass("plannerTB");
     $(".container").append(newTable);
@@ -24,6 +22,8 @@ for (i = 0; i< scheduleHr.length; i++) {
 
     var newMeetingTD = $("<td>");
     newMeetingTD.addClass("column");
+    newMeetingTD.addClass(compareHr[i]);
+    newMeetingTD.attr("data-name", compareHr[i]);
     newMeetingTD.html("");
     var newMeetingText = $("<textarea>");
     newMeetingText.addClass("textarea " + localHr[i]);
@@ -37,8 +37,35 @@ for (i = 0; i< scheduleHr.length; i++) {
     
 
     newRow.append(newHour, newMeetingTD, buttonTd);
+    newMeetingTD.append(newMeetingText);
+    buttonTd.append(newBtn);
 }
 
+$('.saveBtn').on("click", function(){
+    event.preventDefault();
+    var getId = $(this).data("name"); 
+    var meeting = $("." + getId).val();
+    localStorage.setItem(getId, meeting);
+})
 
+function renderMeetings() {
+    for (i=0; i < scheduleHr.length; i++){
+    var returnMeeting = localStorage.getItem(localHr[i]);
+    $("." + localHr[i] + "").html(returnMeeting);
+    }
 
+}
+renderMeetings();
+
+$(".column").each(function(){
+    var dataName = $(this).attr("data-name");
+    var currentTD = "." + dataName;
+    if (dataName == currentHour){
+        $(currentTD).addClass("present");
+    } else if(dataName < currentHour){
+        $(currentTD).addClass("past");
+    } else if (dataName > currentHour){
+        $(currentTD).addClass("future");
+    }
+});
 
